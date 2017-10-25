19d09995d7953563510d42686ee90c4d12d91da5
Michael Samper <msamper@ufl.edu>
2017-10-25 15:52:26 -0400

    created upload controller

---
+++ modules/core/client/controllers/upload.client.controller.js
@@ -0,0 +1,16 @@
+angular
+.module(‘app’)
+.controller(‘myController’, function (currentUser, $scope, FileUploader, $sce) {
+  $scope.uploader = new FileUploader();
+  var uploadURL = ‘/api/upload/’ + currentUser._id
+  $scope.uploadOptions = {
+    queueLimit: 1,
+    autoUpload: true,
+    url: uploadURL
+  }
+
+  $scope.submit = function(){
+    if (!$scope.uploader.queue[0]) return;
+    $scope.uploader.queue[0].upload();
+  }
+}
