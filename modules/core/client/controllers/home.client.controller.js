d03c4bfdd64d7b574220ce08fb613bbc78eb2fb8
Michael Samper <msamper@ufl.edu>
2017-10-31 10:58:55 -0400

    Revert "Twitter api implemented"

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,36 +1,16 @@
 'use strict';
 
+
 angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$http',
-  function ($scope, Authentication, $http) {
+      function ($scope, Authentication, $http) {
+        // This provides Authentication context.
+        $scope.authentication = Authentication;
 
-  	  window.twttr = (function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0],
-	    t = window.twttr || {};
-	  if (d.getElementById(id)) return t;
-	  js = d.createElement(s);
-	  js.id = id;
-	  js.src = "https://platform.twitter.com/widgets.js";
-	  fjs.parentNode.insertBefore(js, fjs);
-
-	  t._e = [];
-	  t.ready = function(f) {
-	    t._e.push(f);
-	  };
-
-	  return t;
-	  }(document, "script", "twitter-wjs"));
-
-
-	  twttr.widgets.load();
-
-
-    // This provides Authentication context.
-    $scope.authentication = Authentication;
-    
-      var periodicCalling = function() { return $http.get('/test').then(function(response) {
+        var periodicCalling = function() { return $http.get('/test').then(function(response) {
           $scope.sample = response.data;
         });
         };
         setInterval(periodicCalling, 1000);
-  }
+
+      }
 ]);
