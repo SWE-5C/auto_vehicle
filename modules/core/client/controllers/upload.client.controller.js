09dc168b3262ad8b4f07026b3973ae5950dcfab1
Michael Samper <msamper@ufl.edu>
2017-11-15 14:58:39 -0500

    fixed go function

--- modules/core/client/controllers/upload.client.controller.js
+++ modules/core/client/controllers/upload.client.controller.js
@@ -1,13 +1,13 @@
 'use strict';
 
-angular.module('core').controller('UploadTestimonialController', ['$scope', '$http',
-  function ($scope, $http) {
+angular.module('core').controller('UploadTestimonialController', ['$scope', '$http', '$state',
+  function ($scope, $http, $state) {
 
     $scope.info = "Default";
 
       $scope.submit = function (isValid) {
           $scope.error = null;
-
+          console.log("HELLO");
           if (!isValid) {
               $scope.$broadcast('show-errors-check-validity', 'userForm');
 
@@ -17,12 +17,14 @@ angular.module('core').controller('UploadTestimonialController', ['$scope', '$ht
           $http.post('/api/testimonial/submit', $scope.form).success(function (response) {
               // If successful we assign the response to the global user model
               $scope.testimonial = response;
-
+              console.log("POSTED");
               // And redirect to the previous or home page
               $state.go('home' || $state.previous.state.name, $state.previous.params);
           }).error(function (response) {
               $scope.error = response.message;
+              console.log("HELP");
           });
+          //$state.go('home' || $state.previous.state.name, $state.previous.params);
       };
 
     $scope.uploadFile = function() {
