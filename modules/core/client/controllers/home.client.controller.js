47689a28e49e2738297808f678fe3e2c9c8217b4
Kevin Asistores <kasistores@gmail.com>
2017-11-16 13:15:35 -0500

    could fix map

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -6,7 +6,6 @@
     function($scope, Authentication, $http) {
       // This provides Authentication context.
       $scope.authentication = Authentication;
-
       var periodicCalling = function() {
         return $http.get('/test').then(function(response) {
           $scope.sample = response.data;
@@ -15,41 +14,53 @@
         });
       };
       setInterval(periodicCalling, 1000);
+      // $scope.$watch(function(){
+      //     console.log($state.$current.name);
+      // }
 
-      function initMap() {
-        var center = {
-          lat: 29.643971,
-          lng: -82.358410
-        };
-        var vehicle_1 = {
-          lat: 29.643979,
-          lng: -82.358415
-        };
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 14,
-          center: center
-        });
-
-        var marker = new google.maps.Marker({
-          position: center,
-          map: map
-        });
-
-
-        function moveMarker() {
-          if ($scope.sample[1] !== null) {
-            var test_lat = $scope.sample[1].location.lat;
-            var test_lng = $scope.sample[1].location.lng;
-            var latlng = new google.maps.LatLng(test_lat, test_lng);
-            marker.setPosition(latlng);
-          }
-          //console.log(marker.position.lat);
+      // function initMap() {
+      //   var center = {
+      //     lat: 29.643971,
+      //     lng: -82.358410
+      //   };
+      //   var vehicle_1 = {
+      //     lat: 29.643979,
+      //     lng: -82.358415
+      //   };
+      //   var map = new google.maps.Map(document.getElementById('map'), {
+      //     zoom: 14,
+      //     center: center
+      //   });
+      //
+      //   var marker = new google.maps.Marker({
+      //     position: center,
+      //     map: map
+      //   });
+      //
+      //
+      //   function moveMarker() {
+      //     if ($scope.sample[1] !== null) {
+      //       var test_lat = $scope.sample[1].location.lat;
+      //       var test_lng = $scope.sample[1].location.lng;
+      //       var latlng = new google.maps.LatLng(test_lat, test_lng);
+      //       marker.setPosition(latlng);
+      //     }
+      //     //console.log(marker.position.lat);
+      //   }
+      //   setInterval(moveMarker, 1000);
+      //
+      //   //console.log("center: ", $scope.marker2.position);
+      // }
+      // google.maps.event.addDomListener(window, "load", initMap);
+      $scope.initialize = function(){
+        $scope.mapOptions = {
+            zoom: 4,
+            center: new google.maps.LatLng(41.923, 12.513),
+            mapTypeId: google.maps.MapTypeId.TERRAIN
         }
-        setInterval(moveMarker, 1000);
 
-        //console.log("center: ", $scope.marker2.position);
+        $scope.map = new google.maps.Map(document.getElementById('map'), $scope.mapOptions);
       }
-      google.maps.event.addDomListener(window, "load", initMap);
 
 
 
