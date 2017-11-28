32529fc247150e4cc223f8ca47394f1c63f0bce3
sperez5553 <sperez5553@gmail.com>
2017-11-27 21:18:29 -0500

    Created Model

---
+++ modules/users/server/models/vehicle.server.model.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Vehicle Schema
+ */
+var VehicleSchema = new Schema({
+  vehicle1ID: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  vehicle2ID: {
+    type: String,
+    trim: true,
+    default: ''
+  },
+  vehicle3ID: {
+    type: String,
+    trim: true,
+    default: ''
+  }
+});
+
+mongoose.model('Testimonial', TestimonialSchema);
